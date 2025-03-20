0cbd266b76f4097dc03e8996acddd5dd24d86ba4
ZyPLJ <1767992919@qq.com>
2025-03-20 22:10:12 +0800

    feat: 修复文章内容标题中带代码块，导致生成Toc目录bug问题

--- Personalblog.Model/Extensions/Markdown/Toc.cs
+++ Personalblog.Model/Extensions/Markdown/Toc.cs
@@ -1,5 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
 using Personalblog.Model.Entitys;
 
 namespace Personalblog.Model.Extensions.Markdown;
@@ -24,8 +26,24 @@ public static class ToC {
         var headings = new List<Heading>();
 
         foreach (var heading in document.Descendants<HeadingBlock>()) {
-            var item = new Heading {Level = heading.Level, Text = heading.Inline?.FirstChild?.ToString()};
-            headings.Add(item);
+            Heading item;  
+            // 处理标题中包含代码块 `标题`
+            if (heading.Inline != null && heading.Inline.Count() > 1) {  
+                var textBuilder = new StringBuilder();  
+                foreach (var inline in heading.Inline) {  
+                    if (inline is CodeInline codeInline)  
+                        textBuilder.Append(codeInline.Content);  
+                    else
+                        textBuilder.Append(inline);  
+                }  
+                // 去除代码块中的空格和换行符
+                string cleanedText = Regex.Replace(textBuilder.ToString().Trim(), @"[\t\n\r]", "");  
+                item = new Heading { Level = heading.Level, Text = cleanedText };  
+            } else {  
+                item = new Heading { Level = heading.Level, Text = heading.Inline?.FirstChild?.ToString() };  
+            }  
+    
+            headings.Add(item);  
         }
 
         var chineseTitleCount = 0;
@@ -69,83 +87,10 @@ public static class ToC {
             var nodes = headings.Where(a => a.Pid == pid).ToList();
             return nodes.Count == 0
                 ? null
-                : nodes.Select(a => new TocNode {Text = a.Text, Href = $"#{a.Slug}", Nodes = GetNodes(a.Id)}).ToList();
+                : nodes.Select(a => new TocNode {Text = a.Text.ToString(), Href = $"#{a.Slug}", Nodes = GetNodes(a.Id)}).ToList();
         }
 
         return GetNodes();
-        //
-        // var headings = new List<Heading>();
-        //
-        // foreach (var heading in document.Descendants<HeadingBlock>()) {
-        //     var item = new Heading {Level = heading.Level, Text = heading.Inline?.FirstChild?.ToString()};
-        //     headings.Add(item);
-        // }
-        //
-        // var slugCounts = new Dictionary<string, int>();
-        // int chineseTitleCount = 0;
-        // for (var i = 0; i < headings.Count; i++) {
-        //     var item = headings[i];
-        //     item.Id = i;
-        //
-        //     var text = item.Text ?? "";
-        //     if (Regex.IsMatch(text, "[\u4e00-\u9fbb]") && !Regex.IsMatch(text, "[A-Za-z]+")) {
-        //         item.Slug = chineseTitleCount == 0 ? "section" : $"section-{chineseTitleCount}";
-        //         chineseTitleCount++;
-        //     }
-        //     else {
-        //         // 使用正则表达式匹配英文字符和空格
-        //         var regex = new Regex(@"[A-Za-z\s]+");
-        //         var matches = regex.Matches(text);
-        //         var filteredText = string.Concat(matches);
-        //
-        //         // 将连续的空格替换为单个空格，并将结果转换为小写
-        //         filteredText = Regex.Replace(filteredText, @"\s+", " ").ToLower();
-        //
-        //         // 移除开头和结尾的空格
-        //         filteredText = filteredText.Trim();
-        //
-        //         // 将剩余的空格替换为连字符（-）
-        //         filteredText = filteredText.Replace(" ", "-");
-        //
-        //         item.Slug = filteredText;
-        //     }
-        //
-        //     // 更新Pid以考虑子目录
-        //     for (var j = i; j >= 0; j--) {
-        //         var preItem = headings[j];
-        //         if (item.Level == preItem.Level + 1) {
-        //             item.Pid = j;
-        //             // if (item.Slug.StartsWith(preItem.Slug)) {
-        //             //     item.Slug = $"{preItem.Slug}-{slugCounts[preItem.Slug]}";
-        //             // }
-        //             break;
-        //         }
-        //     }
-        //
-        //
-        //     // 检查并处理同名目录
-        //     if (slugCounts.ContainsKey(item.Slug)) {
-        //         slugCounts[item.Slug]++;
-        //     } else {
-        //         slugCounts[item.Slug] = 1;
-        //     }
-        //
-        //     // 为同级同名目录添加计数，从1开始，第一个目录不需要编号
-        //     if (slugCounts[item.Slug] > 1 && (item.Pid != null && item.Pid < headings.Count && headings[item.Id].Slug.StartsWith(item.Slug))) {
-        //         item.Slug = $"{item.Slug}-{slugCounts[item.Slug] - 1}";
-        //     }   
-        // }
-        //
-        //
-        //
-        // List<TocNode>? GetNodes(int pid = -1) {
-        //     var nodes = headings.Where(a => a.Pid == pid).ToList();
-        //     return nodes.Count == 0
-        //         ? null
-        //         : nodes.Select(a => new TocNode {Text = a.Text, Href = $"#{a.Slug}", Nodes = GetNodes(a.Id)}).ToList();
-        // }
-        //
-        // return GetNodes();
     }
 
     public static List<TocNode>? ExtractToc(this Post post) {
